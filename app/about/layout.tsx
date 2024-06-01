4aea4b6a80bcde230f4a7e252b4ddfb0927df2e7
Gavin <gavin.a.gregory@gmail.com>
2024-06-01 13:18:57 -0700

    initial commit

---
+++ app/about/layout.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from "react";
+
+export default function AboutLayout({ children }: PropsWithChildren) {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg text-center justify-center">
+        {children}
+      </div>
+    </section>
+  );
+}
