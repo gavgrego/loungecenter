522547b120958b9940f4c728f136d45bc7e58882
Gavin <gavin.a.gregory@gmail.com>
2024-06-21 00:12:58 +0100

    add clerk, small footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,15 @@
 const Footer = () => {
   return (
     <footer className="w-full flex items-center justify-center py-3 bg-secondary-400">
-      <div>Made in Marin County, CA 🌁</div>
+      <div className="flex flex-col gap-4 justify-center light light:text-white">
+        <div>Made in Marin County, CA 🌁</div>
+        <p className="text-sm">
+          We are still growing our dataset! If you see any missing or wrong
+          info, please send us the info (with a source to the correction) using
+          our contact form. You will be automatically entered to win LoungeVault
+          merch!
+        </p>
+      </div>
     </footer>
   );
 };
