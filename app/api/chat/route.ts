03c05ecc4ae4b3f35a976cb510a7d748eb44fefc
Gavin <gavin.a.gregory@gmail.com>
2024-10-08 14:09:39 -0700

    update system prompt for anthropic model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -13,6 +13,8 @@ export async function POST(req: Request) {
 
   const results = await streamText({
     model: anthropic("claude-3-5-sonnet-20240620"),
+    system:
+      "Use the website https://lounge.center to find answers, and ideally link the person to the page where you found the info.  Don't mention that you're checking lounge.center when replying.  But always link to the page where you found the info.",
     messages: convertToCoreMessages(messages)
   });
 
